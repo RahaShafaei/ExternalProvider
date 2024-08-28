8ec5af74a999f58ff57e331536044ae68fda8709
Raha <fatemeshafaei@gmail.com>
2024-08-28 16:16:58 +0330

    Create Project

---
+++ src/main/java/com/example/externalprovider/ExternalProviderApplication.java
@@ -0,0 +1,14 @@
+package com.example.externalprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalProviderApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/example/externalprovider/ExternalProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
